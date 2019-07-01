85b99462a9035a37a341a6fb98958e137c0c76e8
barthelemy <barthelemycourty@gmail.com>
2019-07-01 14:32:56 +0200

    feat(event entity): make event form

---
+++ src/Form/EventType.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Event;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('dateEvent', DateType::class, [
+                'label' => 'Date du concert',
+                'format' => 'dd-MM-yyyy',
+                'widget' => 'choice'
+            ])
+            ->add('startHour', TimeType::class, [
+                'label' => 'Heure de début',
+                'widget' => 'choice'
+            ])
+            ->add('endHour', TimeType::class, [
+                'label' => "Heure de fin",
+                'widget' => 'choice'
+            ])
+            ->add('arrivalHour', TimeType::class, [
+                'label' => "Heure d'arrivée",
+                'widget' => 'choice'
+            ])
+            ->add('allowance', IntegerType::class, [
+                'label' => "Cachet"
+            ])
+            ->add('currency', TextType::class, [
+                'label' => "Devise"
+            ])
+            ->add('hasAccommodation', CheckboxType::class, [
+                'label' => "Hébergement prévu ?",
+                'required' => false
+            ])
+            ->add('isPublic', CheckboxType::class, [
+                'label' => "Voulez-vous rendre cette événement public ?",
+                'required' => false
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Event::class,
+        ]);
+    }
+}

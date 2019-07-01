76b9ad512da0aeeb3bb3d95713230380d81ef744
barthelemy <barthelemycourty@gmail.com>
2019-07-01 15:07:52 +0200

    feat(venue entity): make venue form

---
+++ src/Form/VenueType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Venue;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class VenueType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => "Nom du lieu"
+            ])
+            ->add('audienceCapacity', IntegerType::class, [
+                'label' => "Capacité"
+            ])
+            ->add('note', TextareaType::class, [
+                'label' => "Note"
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Venue::class,
+        ]);
+    }
+}

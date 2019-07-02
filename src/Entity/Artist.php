6cfb72f1426a0aadd40045ffefa441c1b90025f2
barthelemy <barthelemycourty@gmail.com>
2019-07-02 16:05:16 +0200

    feat(relation entity): make relation with contact and artist entity

--- src/Entity/Artist.php
+++ src/Entity/Artist.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -21,6 +23,16 @@ class Artist
      */
     private $name;
 
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Relation", mappedBy="artist")
+     */
+    private $relations;
+
+    public function __construct()
+    {
+        $this->relations = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -34,6 +46,37 @@ class Artist
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Relation[]
+     */
+    public function getRelations(): Collection
+    {
+        return $this->relations;
+    }
+
+    public function addRelation(Relation $relation): self
+    {
+        if (!$this->relations->contains($relation)) {
+            $this->relations[] = $relation;
+            $relation->setArtist($this);
+        }
+
+        return $this;
+    }
+
+    public function removeRelation(Relation $relation): self
+    {
+        if ($this->relations->contains($relation)) {
+            $this->relations->removeElement($relation);
+            // set the owning side to null (unless already changed)
+            if ($relation->getArtist() === $this) {
+                $relation->setArtist(null);
+            }
+        }
 
         return $this;
     }

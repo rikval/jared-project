8495d3eff1f8e2c4c55e44d42f672d4da407c13f
barthelemy <barthelemycourty@gmail.com>
2019-07-02 16:04:06 +0200

    feat(relation entity): make relation entity
    - entity file
    - migration file
    - repository file

---
+++ src/Repository/RelationRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Relation;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method Relation|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Relation|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Relation[]    findAll()
+ * @method Relation[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class RelationRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, Relation::class);
+    }
+
+    // /**
+    //  * @return Relation[] Returns an array of Relation objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('r')
+            ->andWhere('r.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('r.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Relation
+    {
+        return $this->createQueryBuilder('r')
+            ->andWhere('r.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}

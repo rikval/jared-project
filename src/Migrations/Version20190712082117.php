851bbdb76395e940dc610119eccf931388e2ea91
RikVal <rkvalara@gmail.com>
2019-07-12 10:22:37 +0200

    build(tattali/calendar-bundle) : migration of event entity update

---
+++ src/Migrations/Version20190712082117.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190712082117 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE event ADD title VARCHAR(60) NOT NULL, ADD begin_at DATETIME NOT NULL, ADD end_at DATETIME DEFAULT NULL, DROP date_event, DROP start_hour, DROP end_hour');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE event ADD date_event DATE NOT NULL, ADD start_hour TIME DEFAULT NULL, ADD end_hour TIME DEFAULT NULL, DROP title, DROP begin_at, DROP end_at');
+    }
+}

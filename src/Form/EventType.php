38872220ee92e173656ace1e094ac0620b7d3b12
RikVal <rkvalara@gmail.com>
2019-07-24 08:49:24 +0200

    feat(template) : minors fix

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -7,8 +7,6 @@ use App\Entity\Tour;
 use App\Entity\Venue;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\CurrencyType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
